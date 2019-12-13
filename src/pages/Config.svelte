7fd6e67f1d48c8445ce941029507492933dcb7d3
thiagosfideles <43297316+thiagosfideles@users.noreply.github.com>
2019-12-13 12:31:09 -0300

    Create Config.svelte

---
+++ src/pages/Config.svelte
@@ -0,0 +1,62 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+<div class="container-fluid px-xl-5">
+  <section class="py-5">
+    <div class="row text-dark">
+      <div class="col-lg-4 mb-4 mb-lg-0">
+        <div class="card rounded credit-card bg-hover-gradient-violet">
+          <div class="content d-flex flex-column justify-content-between p-4">
+            <h1 class="mb-5">
+              <i class="fab fa-cc-visa" />
+            </h1>
+            <div class="d-flex justify-content-between align-items-end pt-3">
+              <div class="text-uppercase">
+                <div class="font-weight-bold d-block">Card Number</div>
+                <small class="text-gray">1245 1478 1362 6985</small>
+              </div>
+              <h4 class="mb-0">$417.78</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 mb-4 mb-lg-0">
+        <div class="card rounded credit-card bg-hover-gradient-blue">
+          <div class="content d-flex flex-column justify-content-between p-4">
+            <h1 class="mb-5">
+              <i class="fab fa-cc-mastercard" />
+            </h1>
+            <div class="d-flex justify-content-between align-items-end pt-3">
+              <div class="text-uppercase">
+                <div class="font-weight-bold d-block">Card Number</div>
+                <small class="text-gray">1245 1478 1362 6985</small>
+              </div>
+              <h4 class="mb-0">$124.17</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 mb-4 mb-lg-0">
+        <div class="card rounded credit-card bg-hover-gradient-green">
+          <div class="content d-flex flex-column justify-content-between p-4">
+            <h1 class="mb-5">
+              <i class="fab fa-cc-discover" />
+            </h1>
+            <div class="d-flex justify-content-between align-items-end pt-3">
+              <div class="text-uppercase">
+                <div class="font-weight-bold d-block">Card Number</div>
+                <small class="text-gray">1245 1478 1362 6985</small>
+              </div>
+              <h4 class="mb-0">$568.00</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
