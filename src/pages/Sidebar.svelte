6493cd10d8a928679c5d1e86169a9e6807c8676a
thiagosfideles <43297316+thiagosfideles@users.noreply.github.com>
2019-12-13 12:30:42 -0300

    Update Sidebar.svelte
    Add config and dashboard

--- src/pages/Sidebar.svelte
+++ src/pages/Sidebar.svelte
@@ -44,45 +44,23 @@
       </a>
     </li>
     <li class="sidebar-list-item">
-      <a href="." class="sidebar-link text-muted">
-        <i class="o-survey-1 mr-3 text-gray" />
-        <span>Forms</span>
+      <a
+        href="config"
+        on:click={handleMenu}
+        id="config"
+        class="sidebar-link text-muted">
+        <i class="o-settings-window-1 mr-3 text-gray" />
+        <span>Administração</span>
       </a>
     </li>
     <li class="sidebar-list-item">
       <a
-        href="."
-        data-toggle="collapse"
-        data-target="#pages"
-        aria-expanded="false"
-        aria-controls="pages"
+        href="dashboard"
+        on:click={handleMenu}
+        id="dashboard"
         class="sidebar-link text-muted">
-        <i class="o-wireframe-1 mr-3 text-gray" />
-        <span>Pages</span>
-      </a>
-      <div id="pages" class="collapse">
-        <ul
-          class="sidebar-menu list-unstyled border-left border-primary
-          border-thick">
-          <li class="sidebar-list-item">
-            <a href="." class="sidebar-link text-muted pl-lg-5">Page one</a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="." class="sidebar-link text-muted pl-lg-5">Page two</a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="." class="sidebar-link text-muted pl-lg-5">Page three</a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="." class="sidebar-link text-muted pl-lg-5">Page four</a>
-          </li>
-        </ul>
-      </div>
-    </li>
-    <li class="sidebar-list-item">
-      <a href="." class="sidebar-link text-muted">
-        <i class="o-exit-1 mr-3 text-gray" />
-        <span>Login</span>
+        <i class="o-sales-up-1 mr-3 text-gray" />
+        <span>Dashboard</span>
       </a>
     </li>
   </ul>
