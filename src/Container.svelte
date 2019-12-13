1e97b828fb5dccdb99ec14be25b9b8f70cb54c0c
thiagosfideles <43297316+thiagosfideles@users.noreply.github.com>
2019-12-13 12:29:44 -0300

    Update Container.svelte
    Add config and dashboard

--- src/Container.svelte
+++ src/Container.svelte
@@ -2,6 +2,8 @@
   export let content;
   import Home from "./pages/Home.svelte";
   import Calendar from "./pages/Calendar.svelte";
+  import Config from "./pages/Config.svelte";
+  import Dashboard from "./pages/Dashboard.svelte";
 </script>
 
 <style>
@@ -12,4 +14,8 @@
   <Home />
 {:else if content === 'calendar'}
   <Calendar />
+{:else if content === 'config'}
+  <Config />
+{:else if content === 'dashboard'}
+  <Dashboard />
 {/if}
