f1f5622a26bb72899824faeb0e60e6ee2a0f646e
thiagosfideles <43297316+thiagosfideles@users.noreply.github.com>
2019-12-13 12:31:25 -0300

    Create Dashboard.svelte

---
+++ src/pages/Dashboard.svelte
@@ -0,0 +1,27 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+<div class="container-fluid px-xl-5">
+  <section class="py-5">
+    <div class="col mb-1 mx-auto">
+      <div class="card">
+        <div class="card-header">
+          <h6 class="text-uppercase mb-0">Agendamento de aulas</h6>
+        </div>
+        <div class="card-body">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor eum
+            doloremque accusamus blanditiis natus voluptate soluta! Iste optio,
+            vel debitis qui blanditiis omnis cumque rem facere atque temporibus
+            architecto quisquam?
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
